a2459c24ede8c62dbc21f87f9629771b7705376e
jasmineflanders <jasminejoyflanders@gmail.com>
2022-03-21 23:17:53 -0400

    started third tab methodss

--- src/main/java/com/example/bank/BankTellerController.java
+++ src/main/java/com/example/bank/BankTellerController.java
@@ -1,14 +1,47 @@
 package com.example.bank;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
 
 public class BankTellerController {
-    @FXML
-    private Label welcomeText;
+
+    private AccountDatabase bankDatabase;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private Button printAllAccounts;
+
+    @FXML
+    private Button printAllAccountsByType;
+
+    @FXML
+    private Button calculateInterestAndFees;
+
+    @FXML
+    private Button applyInterestsAndFees;
+
+    /*protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+
+    @FXML
+    protected void onPrintAllAccountsButtonClick(ActionEvent event) {
+        bankDatabase.print();
+    }
+
+    @FXML
+    protected void onPrintAllAccountsByTypeButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void onCalculateInterestsAndFeesButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void onApplyInterestsAndFeesButtonClick(ActionEvent event) {
+
     }
 }
